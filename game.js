2e893da306b1260b1b6894c169ec8d36f0efc3aa
rexek60 <rexeko601@gmail.com>
2025-11-28 18:45:36 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,15 +1,1 @@
-
-const c=document.getElementById("game");
-const ctx=c.getContext("2d");
-
-const n=localStorage.getItem("pengu_name");
-const w=localStorage.getItem("pengu_wallet");
-
-function loop(){
-  ctx.clearRect(0,0,900,500);
-  ctx.font="24px Arial";
-  ctx.fillText("Hoşgeldin: "+n,20,40);
-  ctx.fillText("Cüzdan: "+w,20,80);
-  requestAnimationFrame(loop);
-}
-loop();
+console.log('pengu game running');
