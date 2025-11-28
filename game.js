e584a72703a2ef3e02bfb8b315aed6ddd8cd447e
rexek60 <rexeko601@gmail.com>
2025-11-28 14:34:00 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,18 +1,15 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas=document.getElementById("game");
+const ctx=canvas.getContext("2d");
 
-let wallet = localStorage.getItem("pengu_wallet");
-let name = localStorage.getItem("pengu_name");
+let wallet=localStorage.getItem("pengu_wallet");
+let name=localStorage.getItem("pengu_name");
 
-function loop() {
-    ctx.clearRect(0, 0, 900, 500);
-    ctx.font = "24px Arial";
-    ctx.fillStyle = "#000";
-
-    ctx.fillText("Hoşgeldin: " + name, 20, 40);
-    ctx.fillText("Cüzdan: " + wallet, 20, 80);
-
-    requestAnimationFrame(loop);
+function loop(){
+ ctx.clearRect(0,0,900,500);
+ ctx.font="24px Arial";
+ ctx.fillStyle="black";
+ ctx.fillText("Hoşgeldin: "+name,20,40);
+ ctx.fillText("Cüzdan: "+wallet,20,80);
+ requestAnimationFrame(loop);
 }
-
 loop();
