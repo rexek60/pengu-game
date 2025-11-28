ae46733b70ce5b12387cf8fec3e62732610880f3
rexek60 <rexeko601@gmail.com>
2025-11-28 18:02:22 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,12 +1,1 @@
-const canvas=document.getElementById("game");
-const ctx=canvas.getContext("2d");
-
-let pengu=new Image();
-pengu.src="assets/pengu.png";
-
-function loop(){
-    ctx.clearRect(0,0,900,500);
-    ctx.drawImage(pengu,100,300,80,80);
-    requestAnimationFrame(loop);
-}
-pengu.onload = loop;
+console.log('pengu game');
