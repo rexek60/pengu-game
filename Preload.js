8c96a807ad13ae4e0b1ad88b1c616a2ffe7d5bd8
rexek60 <rexeko601@gmail.com>
2025-11-28 22:51:28 +0300

    Delete Preload.js

--- Preload.js
+++
@@ -1,7 +0,0 @@
-class Preload extends Phaser.Scene {
- constructor(){ super('Preload'); }
- preload(){
-   this.load.image('pengu_idle','assets/pengu_idle.png');
- }
- create(){ this.scene.start('Menu'); }
-}
