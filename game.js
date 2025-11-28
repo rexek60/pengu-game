bec30a52352d88a750b23841aad9f7ba56f47fd5
rexek60 <rexeko601@gmail.com>
2025-11-28 16:20:50 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,15 +1,1 @@
-const canvas=document.getElementById("game");
-const ctx=canvas.getContext("2d");
-
-let wallet=localStorage.getItem("pengu_wallet");
-let name=localStorage.getItem("pengu_name");
-
-function loop(){
- ctx.clearRect(0,0,900,500);
- ctx.font="24px Arial";
- ctx.fillStyle="black";
- ctx.fillText("Hoşgeldin: "+name,20,40);
- ctx.fillText("Cüzdan: "+wallet,20,80);
- requestAnimationFrame(loop);
-}
-loop();
+console.log('Pixel Pengu engine placeholder loaded');
