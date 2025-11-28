fbd463bf6f78fee263ed8623cf0d4da8c2ac83a3
rexek60 <rexeko601@gmail.com>
2025-11-28 22:51:36 +0300

    Delete SkinShop.js

--- SkinShop.js
+++
@@ -1,7 +0,0 @@
-class SkinShop extends Phaser.Scene {
- constructor(){ super('SkinShop'); }
- create(){
-   this.add.text(100,100,'Skin Shop',{fontSize:'40px',fill:'#fff'});
-   this.input.once('pointerdown',()=>{ this.scene.start('Game'); });
- }
-}
