08557911db4cd72d529da65c1f9669a387e853c0
rexek60 <rexeko601@gmail.com>
2025-11-28 19:02:41 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
-console.log('pengu game running');
+const c=document.getElementById("game");
+const ctx=c.getContext("2d");
+
+let bg=new Image(); bg.src="assets/background.png";
+let pengu=new Image(); pengu.src="assets/pengu.png";
+
+function loop(){
+ ctx.clearRect(0,0,900,500);
+ ctx.drawImage(bg,0,0,900,500);
+ ctx.drawImage(pengu,100,350,64,64);
+
+ ctx.font="24px Arial";
+ ctx.fillStyle="black";
+ ctx.fillText("Hoşgeldin: "+localStorage.getItem("pengu_name"),20,40);
+ ctx.fillText("Cüzdan: "+localStorage.getItem("pengu_wallet"),20,80);
+
+ requestAnimationFrame(loop);
+}
+pengu.onload=loop;
